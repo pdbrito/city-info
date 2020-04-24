7556b1b5b4dbef422adac9e83d38346c232aa7ba
Pedro de Brito <ped.de.brito@gmail.com>
2020-04-24 17:48:18 +0200

    Add handler testcase for WeatherService errors

--- internal/http/handler_test.go
+++ internal/http/handler_test.go
@@ -2,6 +2,7 @@ package http_test
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/google/go-cmp/cmp"
 	"github.com/pdbrito/city-info/internal/cityinfoservice"
@@ -12,39 +13,40 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strings"
 	"testing"
 )
 
 type weatherService struct {
-	shouldError bool
-	willReturn  weatherservice.Weather
+	err        error
+	willReturn weatherservice.Weather
 }
 
 func (w weatherService) CityWeather(name string) (weatherservice.Weather, error) {
-	return w.willReturn, nil
+	return w.willReturn, w.err
 }
 
-func newWeatherService(shouldError bool, weather weatherservice.Weather) weatherService {
+func newWeatherService(err error, weather weatherservice.Weather) weatherService {
 	return weatherService{
-		shouldError: shouldError,
-		willReturn:  weather,
+		err:        err,
+		willReturn: weather,
 	}
 }
 
 type wikiService struct {
-	shouldError bool
-	willReturn  string
+	err        error
+	willReturn string
 }
 
-func newWikiService(shouldError bool, description string) wikiService {
+func newWikiService(err error, description string) wikiService {
 	return wikiService{
-		shouldError: shouldError,
-		willReturn:  description,
+		err:        err,
+		willReturn: description,
 	}
 }
 
 func (w wikiService) CityDescription(name string) (string, error) {
-	return w.willReturn, nil
+	return w.willReturn, w.err
 }
 
 func TestHandler_GetCityInfo(t *testing.T) {
@@ -56,7 +58,7 @@ func TestHandler_GetCityInfo(t *testing.T) {
 	}
 
 	ws := newWeatherService(
-		false,
+		nil,
 		weatherservice.Weather{
 			Temperature: expectedTemp,
 			Description: cinfo.WeatherSituation,
@@ -64,7 +66,7 @@ func TestHandler_GetCityInfo(t *testing.T) {
 	)
 
 	wis := newWikiService(
-		false,
+		nil,
 		cinfo.Description,
 	)
 
@@ -112,5 +114,61 @@ func TestHandler_GetCityInfo(t *testing.T) {
 			),
 		)
 	}
+}
 
+func TestHandler_GetCityInfo_ReturnsErrorResponseWhenWeatherServiceErrors(t *testing.T) {
+	wsErr := "something went wrong"
+	ws := newWeatherService(
+		errors.New(wsErr),
+		weatherservice.Weather{},
+	)
+
+	wis := newWikiService(
+		nil,
+		"Mos Eisley, you will never find a more wretched hive of scum and villainy.",
+	)
+
+	cis := cityinfoservice.NewCityInfoService(ws, wis, *logrus.New())
+
+	handler := internalHttp.NewHandler(cis)
+	server := httptest.NewServer(handler)
+
+	queryURL := fmt.Sprintf(
+		"%s/city-info?name=%s",
+		server.URL,
+		url.QueryEscape("Mos Eisley"),
+	)
+
+	res, err := http.Get(queryURL)
+
+	if err != nil {
+		t.Fatalf("err getting city info from handler; got %s, want nil", err)
+	}
+
+	if res.StatusCode != http.StatusNotFound {
+		t.Fatalf(
+			"err getting city info from handler; got http status code %d, want %d",
+			res.StatusCode,
+			http.StatusNotFound,
+		)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("err reading response body; got %v, want nil", err)
+	}
+
+	var errResp internalHttp.ErrorResponse
+	err = json.Unmarshal(body, &errResp)
+	if err != nil {
+		t.Fatalf("err unmarshalling json response; got %v want nil", err)
+	}
+
+	if !strings.Contains(errResp.Error, wsErr) {
+		t.Fatalf(
+			"unexpected error message in error response; got %v, want %v",
+			errResp.Error,
+			wsErr,
+		)
+	}
 }
